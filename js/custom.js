827de8be6626c9d3d5002ac7baa52f76d21f7d99
mayur-412 <mayurzezariya990@gmail.com>
2026-01-24 15:53:53 -0800

    add
    add

--- js/custom.js
+++ js/custom.js
@@ -23,3 +23,18 @@ document.addEventListener(
                     });
                 }
             );
+
+
+$('.slick-carousel').slick({
+  centerMode: true,
+  slidesToShow: 3,
+  centerPadding: '60px',
+  dots: false,
+  arrows:false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  pauseOnHover: true,
+  pauseOnDotsHover: true,
+  edgeFriction: 0.05,
+  infinite: true, 
+});
