0a30a954421de13df0ca020845666d215bb4e765
mayur-412 <mayurzezariya990@gmail.com>
2026-01-27 12:00:52 -0800

    add
    add

--- js/custom.js
+++ js/custom.js
@@ -30,11 +30,39 @@ $('.slick-carousel').slick({
   centerMode: true,
   slidesToShow: 3,
   dots: false,
-  // autoplay: true,
+  autoplay: true,
   arrows: false,
   autoplaySpeed: 3000,
   pauseOnHover: true,
   pauseOnDotsHover: true,
   edgeFriction: 0.05,
   infinite: true, 
+   responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
